c67716e43bf8add47502ac8a4064215adc753ebb
Yahya Aalaila <yahya.aalaila@um6p.ma>
2025-04-30 09:42:52 +0200

    restructure files

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     name="neural-stpp",               # ← must match #egg=neural-stpp
     version="0.1.0",                  # ← pick a version
     description="Neural STPP implementation from facebookresearch",
-    packages=find_packages(),         # ← auto-discovers the neural_stpp package
-    ext_modules=cythonize("data_utils_fast.pyx"),
+    packages=find_packages(where="."),         # ← auto-discovers the neural_stpp package
+    ext_modules=cythonize("neural_stpp/data_utils_fast.pyx"),
     include_dirs=[np.get_include()],
     install_requires=[
         "numpy>=1.18",                # ensure runtime NumPy
